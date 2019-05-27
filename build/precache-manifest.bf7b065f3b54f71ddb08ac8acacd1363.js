61e4ea7d60adf8c9c2871e700abf7c2b2f4819d6
caingocthanh <44594034+caingocthanh@users.noreply.github.com>
2019-05-27 20:46:23 +0700

    commit source ORG

---
+++ build/precache-manifest.bf7b065f3b54f71ddb08ac8acacd1363.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbc2585c32bab022bdf2eab60ec377e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bddc03636e284b3b9134",
+    "url": "/static/css/main.40c23932.chunk.css"
+  },
+  {
+    "revision": "20d1bb46ff6c583b9b3b",
+    "url": "/static/js/2.5fd1c813.chunk.js"
+  },
+  {
+    "revision": "bddc03636e284b3b9134",
+    "url": "/static/js/main.5035bf56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
